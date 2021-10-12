2467d0a2be040d56cea32e120bb39d30f3ea36d4
Young <andreasyoungandersen@gmail.com>
2021-10-12 17:21:01 +0200

    Get Family working

---
+++ RestAPI/Controllers/FamilyController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestAPI.FileData;
+using RestAPI.Models;
+
+namespace RestAPI.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class FamilyController : ControllerBase
+	{
+		private FileContext _fileContext;
+		private IList<Family> _families;
+
+		public FamilyController()
+		{
+			_fileContext = new();
+			Console.WriteLine(_fileContext.Families.Count);
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<IList<Family>>> Get([FromQuery] string street, [FromQuery] int? houseNumber)
+		{
+			try
+			{
+				// Set Family List to be all families
+				_families = _fileContext.Families;
+
+				// Check if Street Name has been set
+				if (street != null)
+				{
+					// Remove all Families living on any other street
+					_families = _families.Where(f => f.StreetName == street).ToList();
+				}
+
+				// Check if House Number has been set
+				if (houseNumber != null)
+				{
+					// Remove all Families living in a different house number
+					_families = _families.Where(f => f.HouseNumber == houseNumber).ToList();
+				}
+
+				return _families.Count == 0 ? NotFound(_families) : Ok(_families);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"Caught Exception {e.Message}");
+				return Problem(e.Message);
+			}
+		}
+
+		[HttpDelete]
+		public async Task<ActionResult> Delete()
+		{
+			return Ok();
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> Post([FromQuery] int? id)
+		{
+			return Ok();
+		}
+
+		[HttpPatch]
+		public async Task<ActionResult> Patch([FromQuery] int? id)
+		{
+			return Ok();
+		}
+
+		[HttpPut]
+		public async Task<ActionResult> Put([FromRoute] string street, [FromRoute] int houseNumber)
+		{
+			return Created("", null);
+		}
+	}
+}
---
+++ RestAPI/FileData/FileContext.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using RestAPI.Models;
+
+namespace RestAPI.FileData
+{
+	public class FileContext
+	{
+		public IList<Family> Families { get; private set; }
+
+		private readonly string familiesFile = "families.json";
+
+		public FileContext()
+		{
+			Families = File.Exists(familiesFile) ? ReadData<Family>(familiesFile) : new List<Family>();
+		}
+
+		private IList<T> ReadData<T>(string s)
+		{
+			using (var jsonReader = File.OpenText(familiesFile))
+			{
+				return JsonSerializer.Deserialize<List<T>>(jsonReader.ReadToEnd());
+			}
+		}
+
+		public void SaveChanges()
+		{
+			string jsonFamilies = JsonSerializer.Serialize(Families, new JsonSerializerOptions {WriteIndented = true});
+			using (StreamWriter outputFile = new StreamWriter(familiesFile, false))
+			{
+				outputFile.Write(jsonFamilies);
+			}
+		}
+	}
+}
